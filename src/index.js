096a44d1841ab08ad110113acb2e58ff0e5d60e8
momo setti <zinasetti@gmail.com>
2019-01-27 17:05:29 +0100

    fix errors

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('helloworld')
+import React,{Component} from "react";
+import ReactDom from "react-dom";
+
+
+class HelloWorld extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Helloworld</h1>
+            </div>
+        )
+    }
+}
+
+ReactDom.render(<HelloWorld/>,document.getElementById('root'))
